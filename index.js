949d774066eb1119fbfcc8e3e85f9b2e14addb39
DahyeY <dhuniv@naver.com>
2024-07-07 15:28:45 +0900

    style: 오타 수정

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = 8080;
 
 // app.get("/", (req, res) => res.send("Index.js"));
 
-app.listen(port, () => console.log(`Example app listenint on port ${port}!`));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 
 // const dotenv = require('dotenv');
